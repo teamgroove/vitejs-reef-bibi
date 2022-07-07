39f3d37c776934cfda328079c61e5baa108f0942
teamgroove <gh.rene@teamgroove.de>
2022-07-07 19:24:17 +0200

    1 file modified, 1 deleted

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,8 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: [],
+  content: ['index.html', './src/**/*.{js,jsx,ts,tsx,vue,html}'],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
